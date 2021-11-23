a4319a45afd43817756a79a2194dc9b8c198b68a
Korisss <vaginpawel2005@gmail.com>
2021-11-23 08:29:02 +0300

    fix: add already started check

--- RedHouseLauncher/Program.cs
+++ RedHouseLauncher/Program.cs
@@ -1,7 +1,9 @@
 ﻿using RedHouseLauncher.Core.Settings;
 using System;
+using System.Diagnostics;
 using System.Net;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RedHouseLauncher
 {
@@ -19,8 +21,23 @@ namespace RedHouseLauncher
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls; // Попробовать потом вернуть SSL
 
             App app = new();
+
+            CheckAlreadyStarted();
+
             app.InitializeComponent();
             app.Run();
+        }
+
+        private static void CheckAlreadyStarted()
+        {
+            string exe = AppDomain.CurrentDomain.FriendlyName;
+
+            Process[] launcherProcesses = Process.GetProcessesByName(exe);
+
+            if (launcherProcesses.Length < 2) return;
+
+            MessageBox.Show("Лаунчер уже запущен.");
+            Application.Current.Shutdown();
         }
     }
 }
