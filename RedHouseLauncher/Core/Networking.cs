b96d76337fee4272346f9a24136978d501638ab5
Korisss <vaginpawel2005@gmail.com>
2021-11-24 15:10:07 +0300

    fix: add some Networking overloads and remove sync request

--- RedHouseLauncher/Core/Networking.cs
+++ RedHouseLauncher/Core/Networking.cs
@@ -8,73 +8,14 @@ namespace RedHouseLauncher.Core
 {
     internal static class Networking
     {
-        // Было в старой версии, чекнуть (После проверки auth)
-        // if (data == null && method == "POST" || data != null)
-        //     using (StreamWriter streamWriter = new StreamWriter(httpClient.GetStreamAsync(url).Result))
-        //     {
-        //         streamWriter.Write($"{data ?? ""}");
-        //     }
-
-        internal static string? Request(string url, string method = "GET", string? data = null, bool auth = false)
-        {
-            HttpClient httpClient = new();
-            httpClient.Timeout = TimeSpan.FromSeconds(5);
-            httpClient.BaseAddress = new Uri(url);
-
-            if (auth)
-            {
-                httpClient.DefaultRequestHeaders.Add("Authorization", Settings.Settings.UserToken);
-            }
-
-            if (method == "GET")
-            {
-                using HttpResponseMessage response = httpClient.GetAsync(url).Result;
-                response.EnsureSuccessStatusCode();
-
-                using Stream responseStream = response.Content.ReadAsStream();
-
-                if (responseStream == null)
-                {
-                    return null;
-                }
-
-                using StreamReader streamReader = new(responseStream);
-                string result = streamReader.ReadToEnd();
-
-                return result;
-            }
-            else if (method == "POST")
-            {
-                if (data == null)
-                {
-                    data = "";
-                }
-
-                using HttpContent content = new StringContent(data, Encoding.UTF8, "application/json");
-
-                using HttpResponseMessage response = httpClient.PostAsync(url, content).Result;
-                response.EnsureSuccessStatusCode();
-
-                using Stream responseStream = response.Content.ReadAsStream();
-
-                if (responseStream == null)
-                {
-                    return null;
-                }
-
-                using StreamReader streamReader = new(responseStream);
-                string result = streamReader.ReadToEnd();
-
-                return result;
-
-            }
-
-            return null;
-        }
-
         internal static async Task<string?> RequestAsync(string url, string method = "GET", string? data = null, bool auth = false)
         {
-            HttpClient httpClient = new();
+            using HttpClient httpClient = new();
+            return await RequestAsync(httpClient, url, method, data, auth);
+        }
+
+        internal static async Task<string?> RequestAsync(HttpClient httpClient, string url, string method = "GET", string? data = null, bool auth = false)
+        {
             httpClient.Timeout = TimeSpan.FromSeconds(5);
             httpClient.BaseAddress = new Uri(url);
 
