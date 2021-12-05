cf67e8ce97af91c8ee6674e0f821864f837ee0e8
Korisss <vaginpawel2005@gmail.com>
2021-12-05 16:33:43 +0300

    fix: login model response

--- RedHouseLauncher/Core/Auth/Models/Responses/LoginModelResponse.cs
+++ RedHouseLauncher/Core/Auth/Models/Responses/LoginModelResponse.cs
@@ -4,11 +4,6 @@ namespace RedHouseLauncher.Core.Auth.Models.Responses
 {
     internal class LoginModelResponse
     {
-        internal LoginModelResponse(int id, string token)
-        {
-            Id = id;
-            Token = token;
-        }
         [JsonProperty("id")] internal int Id { get; set; }
         [JsonProperty("token")] internal string Token { get; set; }
     }
