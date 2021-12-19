df437fd86736af9c2769c6e64f85b55ff0d91e2e
Korisss <vaginpawel2005@gmail.com>
2021-12-19 20:35:43 +0300

    feat: start adding serverTool with new design to launcher repo

---
+++ ServerTool/UI/Images/Icons/ExitIcon.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ServerTool.UI.Images.Icons
+{
+    /// <summary>
+    /// Логика взаимодействия для ExitIcon.xaml
+    /// </summary>
+    public partial class ExitIcon : UserControl
+    {
+        public ExitIcon()
+        {
+            InitializeComponent();
+        }
+
+        private void AddHighlight(object sender, MouseEventArgs e)
+        {
+            LineBrush1.Brush = Brushes.White;
+            LineBrush2.Brush = Brushes.White;
+        }
+
+        private void RemoveHighlight(object sender, MouseEventArgs e)
+        {
+            object? colorConverter = ColorConverter.ConvertFromString("#8C8C8C");
+
+            if (colorConverter == null)
+            {
+                return;
+            }
+
+            LineBrush1.Brush = new SolidColorBrush((Color)colorConverter);
+            LineBrush2.Brush = new SolidColorBrush((Color)colorConverter);
+        }
+
+        private void CloseApp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ ServerTool/UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ServerTool.UI.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    DragMove();
+                }
+            }
+            catch { }
+        }
+    }
+}
