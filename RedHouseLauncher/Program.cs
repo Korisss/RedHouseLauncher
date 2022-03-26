2fe974f270c19ca2615143e18d71b63c9ec8d2b2
Korisss <vaginpawel2005@yandex.ru>
2022-03-26 14:18:08 +0300

    test2

--- RedHouseLauncher/Program.cs
+++ RedHouseLauncher/Program.cs
@@ -22,7 +22,7 @@ namespace RedHouseLauncher
         public static void Main(string[] args)
         {
             ServicePointManager.Expect100Continue = true;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3 | SecurityProtocolType.SystemDefault;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
 
             if (args.Length > 0)
             {
